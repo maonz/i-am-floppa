99b8082eec14af604d55e8b2f38e66fe7801a3e6
Maonz <maonz666@gmail.com>
2022-02-08 21:16:38 +0300

    Initial Commit

---
+++ i am floppa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Admin on 08.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
